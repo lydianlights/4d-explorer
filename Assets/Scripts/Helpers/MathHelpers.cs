714164d49f9a1994b2d6fa17635356420db0836a
Rane Fields <rane.fields@gmail.com>
2018-01-29 14:11:57 -0800

    Add tetrahedron that took way longer than it should have

---
+++ Assets/Scripts/Helpers/MathHelpers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Scripts.Helpers
+{
+    public static class MathHelpers
+    {
+        public const float Sqrt2 = 1.41421356237f;
+        public const float Sqrt2Inv = 0.70710678118f;
+    }
+}
---
+++ Assets/Scripts/Shapes/Tetrahedron.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Scripts.Helpers;
+
+namespace Scripts.Shapes
+{
+    class Tetrahedron : Polyhedron
+    {
+        protected override void GenerateVerticesAndEdges()
+        {
+            Vertices = new Vertex[]
+            {
+                new Vertex(Mathf.Sqrt(8f/9f), -1f/3f, 0f),
+                new Vertex(-Mathf.Sqrt(2f/9f), -1f/3f, Mathf.Sqrt(2f/3f)),
+                new Vertex(-Mathf.Sqrt(2f/9f), -1f/3f, -Mathf.Sqrt(2f/3f)),
+                new Vertex(0f, 1f, 0f)
+            };
+
+            Edges = new Edge[]
+            {
+                new Edge(Vertices[0], Vertices[1]),
+                new Edge(Vertices[0], Vertices[2]),
+                new Edge(Vertices[0], Vertices[3]),
+                new Edge(Vertices[1], Vertices[2]),
+                new Edge(Vertices[1], Vertices[3]),
+                new Edge(Vertices[2], Vertices[3]),
+            };
+        }
+    }
+}
