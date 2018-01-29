bbffd171e173a60d29f883a6ecceefd882618d79
Rane Fields <rane.fields@gmail.com>
2018-01-29 14:45:50 -0800

    Add triangular prism

--- Assets/Scripts/Helpers/MathHelpers.cs
+++ Assets/Scripts/Helpers/MathHelpers.cs
@@ -7,5 +7,7 @@ namespace Scripts.Helpers
     {
         public const float Sqrt2 = 1.41421356237f;
         public const float Sqrt2Inv = 0.70710678118f;
+        public const float Sqrt3 = 1.73205080757f;
+        public const float Sqrt3Inv = 0.57735026919f;
     }
 }
---
+++ Assets/Scripts/Shapes/TriangularPrism.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Scripts.Helpers;
+
+namespace Scripts.Shapes
+{
+    public class TriangularPrism : Polyhedron
+    {
+        public float Length = 1;
+
+        protected override void GenerateVerticesAndEdges()
+        {
+            float yOffset = Length / 2;
+            Vertices = new Vertex[]
+            {
+                new Vertex(-MathHelpers.Sqrt3Inv, -yOffset, -1f/3f),
+                new Vertex(MathHelpers.Sqrt3Inv, -yOffset, -1f/3f),
+                new Vertex(0, -yOffset, 2f/3f),
+
+                new Vertex(-MathHelpers.Sqrt3Inv, yOffset, -1f/3f),
+                new Vertex(MathHelpers.Sqrt3Inv, yOffset, -1f/3f),
+                new Vertex(0, yOffset, 2f/3f),
+            };
+
+            Edges = new Edge[]
+            {
+                new Edge(Vertices[0], Vertices[1]),
+                new Edge(Vertices[1], Vertices[2]),
+                new Edge(Vertices[2], Vertices[0]),
+
+                new Edge(Vertices[0], Vertices[3]),
+                new Edge(Vertices[1], Vertices[4]),
+                new Edge(Vertices[2], Vertices[5]),
+
+                new Edge(Vertices[3], Vertices[4]),
+                new Edge(Vertices[4], Vertices[5]),
+                new Edge(Vertices[5], Vertices[3]),
+            };
+        }
+    }
+}
