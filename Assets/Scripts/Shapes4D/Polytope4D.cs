1f3aed79970cf74cd58e6ea259d4a714523b400c
Rane Fields <rane.fields@gmail.com>
2018-01-30 13:58:23 -0800

    Make preliminary 4d data classes

---
+++ Assets/Scripts/Shapes4D/Polytope4D.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Shapes4D
+{
+    public class Polytope4D : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/Scripts/Shapes4D/Vertex4D.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Shapes4D
+{
+    public class Vertex4D
+    {
+        public Vector4 LocalPostion { get; set; }
+
+        // TODO: Global Position
+        public int Index { get; set; }
+        public Polytope4D Parent { get; private set; }
+
+        public Vertex4D(Polytope4D parent, int index, Vector4 position)
+        {
+            Parent = parent;
+            Index = index;
+            LocalPostion = position;
+        }
+
+        public Vertex4D(Polytope4D parent, int index, float x, float y, float z, float w)
+            : this(parent, index, new Vector4(x, y, z, w)) { }
+    }
+}
