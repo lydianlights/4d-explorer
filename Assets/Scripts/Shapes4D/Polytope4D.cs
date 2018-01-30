d983f3867056df6dad08904c742d788d7a990aef
Rane Fields <rane.fields@gmail.com>
2018-01-30 14:19:35 -0800

    Link polytope to 4d transform

--- Assets/Scripts/Shapes4D/Polytope4D.cs
+++ Assets/Scripts/Shapes4D/Polytope4D.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Scripts.Shapes4D;
 
 namespace Scripts.Shapes4D
 {
+    [RequireComponent(typeof(Transform4D))]
     public class Polytope4D : MonoBehaviour
     {
+        [NonSerialized]
+        public Transform4D Transform;
 
+        // Run on script load
+        public void Awake()
+        {
+            Transform = GetComponent<Transform4D>();
+        }
     }
 }
---
+++ Assets/Scripts/Shapes4D/Transform4D.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Shapes4D
+{
+    public class Transform4D : MonoBehaviour
+    {
+        public Vector4 Position { get; set; }
+    }
+}
