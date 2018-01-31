094ea09933d596f9b084ebc4e042c84207c726ec
Rane Fields <rane.fields@gmail.com>
2018-01-31 01:32:11 -0800

    Add 4D orientation class

---
+++ Assets/Scripts/Shapes4D/Orientation4D.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Shapes4D
+{
+    // TODO: Do some fancy math because 6 planes of rotation is redundant... but easy to think about
+    public class Orientation4D
+    {
+        public float XY = 0;
+        public float XZ = 0;
+        public float YZ = 0;
+        public float XW = 0;
+        public float YW = 0;
+        public float ZW = 0;
+
+        public Orientation4D() { }
+
+        public Orientation4D(float xy, float xz, float yz, float xw, float yw, float zw)
+        {
+            XY = xy;
+            XZ = xz;
+            YZ = yz;
+            XW = xw;
+            YW = yw;
+            ZW = zw;
+        }
+    }
+}
