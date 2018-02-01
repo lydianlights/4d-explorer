9e09a2fced7d352cf60315782b293c68edf3fe4e
Rane Fields <rane.fields@gmail.com>
2018-02-01 00:22:38 -0800

    Add player with buggy mouselook

---
+++ Assets/Scripts/Player/PlayerCamera.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scripts.Player
+{
+    [RequireComponent(typeof(Camera))]
+    public class PlayerCamera : MonoBehaviour
+    {
+        // Set in Unity
+        public float Sensitivity = 3f;
+
+        private Camera camera;
+
+        public void Awake()
+        {
+            camera = GetComponent<Camera>();
+        }
+
+        public void Update()
+        {
+            float yaw = camera.transform.localRotation.eulerAngles.y + Input.GetAxis("Mouse X") * Sensitivity;
+            float pitch = camera.transform.localRotation.eulerAngles.x - Input.GetAxis("Mouse Y") * Sensitivity;
+            //pitch = Mathf.Clamp(pitch, -80, 80);
+            float roll = camera.transform.localRotation.eulerAngles.z;
+
+            camera.transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    // Set in Unity
+    public float Gravity = 20f;
+    
+    [NonSerialized]
+    public Vector3 Velocity = new Vector3(0, 0, 0);
+
+    private CharacterController control;
+
+    // Run on script load
+    public void Awake()
+    {
+        control = GetComponent<CharacterController>();
+    }
+
+    // Run on object load
+    public void Start()
+    {
+		
+	}
+
+    // Run every frame
+    public void Update()
+    {
+        
+    }
+
+    // Run every physics frame
+    public void FixedUpdate()
+    {
+        Velocity.y -= Time.deltaTime * Gravity;
+        if (control.isGrounded)
+        {
+            Velocity.y = 0;
+        }
+        control.Move(Time.deltaTime * Velocity);
+    }
+}
