77a8923feb99774af0236ad586bbd3b12dbea9fe
Rane Fields <rane.fields@gmail.com>
2018-02-01 12:43:21 -0800

    Simple player FPS movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -11,15 +11,26 @@ namespace Scripts.Player
         [SerializeField]
         private PlayerCamera cameraPrefab;
         public float Gravity = 20f;
-        public float MoveSpeed = 10f;
+        public float MaxMoveSpeed = 10f;
+        public float Acceleration = 100f;
+        public float JumpStrength = 5f;
+        public float AirAcceleration = 10f;
 
         [NonSerialized]
         public Vector3 Velocity = new Vector3(0, 0, 0);
+        public Vector3 HorizontalVelocity
+        {
+            get
+            {
+                return new Vector3(Velocity.x, 0, Velocity.z);
+            }
+        }
         [NonSerialized]
         public PlayerCamera Camera;
 
         private CharacterController control;
         private Vector3 moveDirection = new Vector3(0, 0, 0);
+        private bool isJumpPressed = false;
 
         // Run on script load
         public void Awake()
@@ -37,6 +48,17 @@ namespace Scripts.Player
         // Run every frame
         public void Update()
         {
+            SetMovementDirectionFromPlayerInput();
+        }
+
+        // Run every physics frame
+        public void FixedUpdate()
+        {
+            MovePlayer();
+        }
+
+        private void SetMovementDirectionFromPlayerInput()
+        {
             var facing = Quaternion.Euler(0, Camera.Yaw, 0);
             moveDirection = facing * new Vector3(
                 Input.GetAxis("Strafe"),
@@ -44,20 +66,61 @@ namespace Scripts.Player
                 Input.GetAxis("Forward")
             );
             moveDirection.Normalize();
+            
+            if (Input.GetButtonDown("Jump") && control.isGrounded)
+            {
+                isJumpPressed = true;
+            }
         }
 
-        // Run every physics frame
-        public void FixedUpdate()
+        private void MovePlayer()
         {
-            Velocity.x = moveDirection.x * MoveSpeed;
-            Velocity.z = moveDirection.z * MoveSpeed;
+            CalculateVelocity();
+            control.Move(Time.deltaTime * Velocity);
+        }
+
+        private void CalculateVelocity()
+        {
+            var acceleration = (control.isGrounded) ? Acceleration : AirAcceleration;
+            if (moveDirection != Vector3.zero)
+            {
+                Velocity += Time.deltaTime * acceleration * moveDirection;
+            }
+            else
+            {
+                // Deccelerate if no player input
+                bool signX = (HorizontalVelocity.x > 0f);
+                bool signZ = (HorizontalVelocity.z > 0f);
+
+                acceleration = -acceleration;
+                Velocity += Time.deltaTime * acceleration * HorizontalVelocity.normalized;
+
+                bool newSignX = (Velocity.x > 0f);
+                bool newSignZ = (Velocity.z > 0f);
+                
+                if (signX != newSignX || signZ != newSignZ)
+                {
+                    Velocity.x = 0;
+                    Velocity.z = 0;
+                }
+            }
+
+            if (HorizontalVelocity.magnitude > MaxMoveSpeed)
+            {
+                float sf = MaxMoveSpeed / HorizontalVelocity.magnitude;
+                Velocity.Scale(new Vector3(sf, 1, sf));
+            }
+
             Velocity.y -= Time.deltaTime * Gravity;
             if (control.isGrounded)
             {
-                Velocity.y = 0;
+                Velocity.y = -control.stepOffset / Time.deltaTime;
+                if (isJumpPressed)
+                {
+                    Velocity.y = JumpStrength;
+                    isJumpPressed = false;
+                }
             }
-
-            control.Move(Time.deltaTime * Velocity);
         }
     }
 }
